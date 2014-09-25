b568d5a053379c3d462baf8adbd265443d9ca754
Daryll Doyle <daryll@digitalwebmedia.co.uk>
2014-09-25 11:51:06 +0100

    Mode connection method added

--- src/Shared.php
+++ src/Shared.php
@@ -1,8 +1,29 @@
 <?php
+/**
+ * Common functions that can be used across all methods
+ */
 
 namespace dwmsw\sagepay;
 
 class Shared extends AbstractSettings
 {
+
+    /**
+     * Sets the connection mode for the API
+     * @param string $mode The mode to connect with
+     */
+    public function setConnectionMode($mode)
+    {
+        $mode = strtolower($mode);
+
+        if (isset($this->directEndPoints[$mode])) {
+            $this->mode = $mode;
+        } else {
+            // Supplied connection mode not supported.
+            throw new \Exception("Invalid connection mode name '{$mode}'");
+        }
+
+        return $this;
+    }
 
 }
