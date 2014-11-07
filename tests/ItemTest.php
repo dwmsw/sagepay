c00dc3f44d58039158b8600a7613df9220dd0998
Daryll Doyle <daryll@digitalwebmedia.co.uk>
2014-11-07 10:56:20 +0000

    Test the item getters

--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -17,6 +17,14 @@ class ItemTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(2, $output['quantity']);
         $this->assertEquals(36, $output['itemTotal']);
         $this->assertEquals(72, $output['lineTotal']);
+
+        // Here we'll test the getters
+        $this->assertEquals('Test Item', $item->getDescription());
+        $this->assertEquals(30.00, $item->getUnitNetAmount());
+        $this->assertEquals(6, $item->getUnitTaxAmount());
+        $this->assertEquals(2, $item->getQuantity());
+        $this->assertEquals(36, $item->getUnitGrossAmount());
+        $this->assertEquals(72, $item->getTotalGrossAmount());
     }
 
 }
