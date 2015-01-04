6abb18cbad24e6393a7e0d5d93f6f0679ed3592d
Daryll Doyle <daryll@digitalwebmedia.co.uk>
2015-01-04 02:54:23 +0000

    Shared exception tests

--- tests/SharedTest.php
+++ tests/SharedTest.php
@@ -133,6 +133,19 @@ class SharedTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('12345', $shared->getVendorTxCode());
     }
 
+    public function testVendorTxCodeException()
+    {
+        $shared = new dwmsw\sagepay\Shared();
+
+        try {
+            $shared->setVendorTxCode('12345jasnfjkdfbjksdfbskf1231nmkasd1knasdasdasdasdasdasdasdasdada');
+        } catch (InvalidArgumentException $expected) {
+            return;
+        }
+
+        $this->fail('An expected exception has not been raised.');
+    }
+
     public function testDescription()
     {
         $shared = new dwmsw\sagepay\Shared();
@@ -140,6 +153,19 @@ class SharedTest extends PHPUnit_Framework_TestCase
         $shared->setDescription('12345');
 
         $this->assertEquals('12345', $shared->getDescription());
+    }
+
+    public function testDescriptionException()
+    {
+        $shared = new dwmsw\sagepay\Shared();
+
+        try {
+            $shared->setDescription('Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, tot');
+        } catch (InvalidArgumentException $expected) {
+            return;
+        }
+
+        $this->fail('An expected exception has not been raised.');
     }
 
     public function testEmail()
@@ -151,6 +177,19 @@ class SharedTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('test@test.test', $shared->getCustomerEmail());
     }
 
+    public function testEmailException()
+    {
+        $shared = new dwmsw\sagepay\Shared();
+
+        try {
+            $shared->setCustomerEmail('Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas');
+        } catch (InvalidArgumentException $expected) {
+            return;
+        }
+
+        $this->fail('An expected exception has not been raised.');
+    }
+
     public function testGiftAid()
     {
         $shared = new dwmsw\sagepay\Shared();
@@ -158,5 +197,67 @@ class SharedTest extends PHPUnit_Framework_TestCase
         $shared->setGiftAid(1);
 
         $this->assertEquals(1, $shared->getGiftAid());
+    }
+
+    public function testGiftAidException()
+    {
+        $shared = new dwmsw\sagepay\Shared();
+
+        try {
+            $shared->setGiftAid(5);
+        } catch (InvalidArgumentException $expected) {
+            return;
+        }
+
+        $this->fail('An expected exception has not been raised.');
+    }
+
+    public function testBillingAddress()
+    {
+        $shared = new dwmsw\sagepay\Shared();
+
+        $shared->setBillingAddress(new dwmsw\sagepay\Address());
+
+        $this->assertEquals(new dwmsw\sagepay\Address(), $shared->getBillingAddress());
+    }
+
+    public function testDeliveryAddress()
+    {
+        $shared = new dwmsw\sagepay\Shared();
+
+        $shared->setDeliveryAddress(new dwmsw\sagepay\Address());
+
+        $this->assertEquals(new dwmsw\sagepay\Address(), $shared->getDeliveryAddress());
+    }
+
+    public function testBasket()
+    {
+        $shared = new dwmsw\sagepay\Shared();
+
+        $shared->setBasket(new dwmsw\sagepay\Basket());
+
+        $this->assertEquals(array(), $shared->getBasketItems(false));
+    }
+
+    public function testTokens()
+    {
+        $shared = new dwmsw\sagepay\Shared();
+
+        $shared->setCreateToken(1);
+
+        $token = $shared->getCreateToken();
+
+        $this->assertEquals(1, $token);
+    }
+
+    public function testCard()
+    {
+        $shared = new dwmsw\sagepay\Shared();
+
+        $shared->setCard(new dwmsw\sagepay\Card());
+
+        $card = $shared->getCard();
+
+        $this->assertEquals(new dwmsw\sagepay\Card(), $card);
     }
 }
