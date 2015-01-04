83241df0c49be12bab9af7eb4921f3fd737f420a
Daryll Doyle <daryll@digitalwebmedia.co.uk>
2015-01-04 02:41:11 +0000

    More basket tests

--- tests/BasketTest.php
+++ tests/BasketTest.php
@@ -3,6 +3,55 @@
 class BasketTest extends PHPUnit_Framework_TestCase
 {
 
+    private $basket;
+
+    public function setUp()
+    {
+        $this->basket = new dwmsw\sagepay\Basket();
+    }
+
+    public function testDeliveryNetAmount()
+    {
+        $this->basket->setDeliveryNetAmount(3.00);
+
+        $amount = $this->basket->getDeliveryNetAmount();
+
+        $this->assertEquals(3.00, $amount);
+    }
+
+    public function testDeliveryTaxAmount()
+    {
+        $this->basket->setDeliveryTaxAmount(.6);
+
+        $amount = $this->basket->getDeliveryTaxAmount();
+
+        $this->assertEquals(0.60, $amount);
+    }
+
+    public function testDeliveryGrossAmount()
+    {
+        $this->basket->setDeliveryNetAmount(3.00);
+        $this->basket->setDeliveryTaxAmount(.6);
+
+        $amount = $this->basket->getDeliveryGrossAmount();
+
+        $this->assertEquals(3.60, $amount);
+    }
+
+    public function testItems()
+    {
+        $this->basket->setDeliveryNetAmount(3.00);
+        $this->basket->setDeliveryTaxAmount(.6);
+
+        $item = new dwmsw\sagepay\Item('Test Item',2.00, .4, 1);
+
+        $this->basket->addItem($item);
+
+        $amount = $this->basket->getAmount();
+
+        $this->assertEquals(6.00, $amount);
+    }
+
     public function testAddToBasket()
     {
         $basket = new dwmsw\sagepay\Basket();
